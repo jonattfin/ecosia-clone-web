aeaa4240bbe7eb9d22e7bff3cc4286373a32c6a8
jonattfin <jonattfin@outlook.com>
2022-05-17 21:45:11 +0300

    wip

--- src/shared-components/footer/index.tsx
+++ src/shared-components/footer/index.tsx
@@ -1,44 +1,36 @@
+import { Grid } from "@mui/material";
+import styled from "@emotion/styled";
+import {
+  FacebookRounded,
+  PublicRounded,
+  EmojiPeopleRounded,
+  PianoRounded,
+} from "@mui/icons-material";
 
-import { Grid } from '@mui/material';
-import styled from '@emotion/styled';
-import { FacebookRounded, PublicRounded, EmojiPeopleRounded, PianoRounded } from '@mui/icons-material';
-
-const DivContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`;
-
-const DivLine = styled.div`
-  border-top: 2px dotted grey;
-  text-align: center;
-  padding: 10px 50px;
-`;
-
-const DivExplanation = styled.div`
-  text-align: center;
-  font-size: small;
-  padding: 10px 0;
-`;
+import styles from "./footer.module.scss";
 
 export default function Component() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} xl={12}>
-        <DivLine />
-        <DivContainer>
-          <FacebookRounded/>  
-          <PublicRounded />
-          <EmojiPeopleRounded />
-          <PianoRounded />
-        </DivContainer>
+    <div className={styles.footer}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} xl={12}>
+          <div className={styles.line} />
+          <div className={styles.container}>
+            <FacebookRounded />
+            <PublicRounded />
+            <EmojiPeopleRounded />
+            <PianoRounded />
+          </div>
+        </Grid>
+        <Grid item xs={12} xl={12}>
+          <div className={styles.explanation}>
+            Ecosia GmbH does not assume responsibility for the content of sites
+            to which it links and the way in which search results are displayed.
+            To learn more please read our privacy policy and our terms of
+            service.
+          </div>
+        </Grid>
       </Grid>
-      <Grid item xs={12} xl={12}>
-        <DivExplanation>
-          Ecosia GmbH does not assume responsibility for the content of sites to which it links and the way in which search results are displayed. To learn more please read our privacy policy and our terms of service.
-        </DivExplanation>
-      </Grid>
-    </Grid>
-  )
+    </div>
+  );
 }
--- src/shared-components/links/index.tsx
+++ src/shared-components/links/index.tsx
@@ -1,79 +1,102 @@
-import { Grid } from '@mui/material';
-import styled from '@emotion/styled';
-import Image from 'next/image';
+import { Grid } from "@mui/material";
+import styled from "@emotion/styled";
+import Image from "next/image";
 
-import images from './images';
-
-const GridWrapper = styled(Grid)`
-  padding: 40px;
-`
-
-const DivContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: flex-start;
-  height: 15vh;
-`;
-
-const ImgWrapper = styled(Image)`
-  width: 10vw;
-  max-width: 150px;
-`;
-
-const DivTitle = styled.div`
-  font-size: medium;
-  text-transform: uppercase;
-  padding-bottom: 20px;
-`
+import images from "./images";
+import styles from "./links.module.scss";
 
 const Links = () => {
   return (
-    <GridWrapper container spacing={2}>
-      <Grid item xs={2} xl={2}>
-        <div><ImgWrapper src={images.corporationImage} alt="b corporation"></ImgWrapper></div>
-      </Grid>
-      <Grid item xs={8} xl={8}>
-        <DivContainer>
+    <div className={styles.links}>
+      <Grid container spacing={2}>
+        <Grid item xs={2} xl={2}>
           <div>
-            <DivTitle>SITEMAP</DivTitle>
-            <div><a href="#id">News</a></div>
-            <div><a href="#id">How Ecosia works</a></div>
-            <div><a href="#id">About us</a></div>
-            <div><a href="#id">Mobile app</a></div>
-            <div><a href="#id">Privacy</a></div>
-            <div><a href="#id">Settings</a></div>
+            <Image
+              className={styles["image-wrapper"]}
+              src={images.corporationImage}
+              alt="b corporation"
+            ></Image>
           </div>
-          <div>
-            <DivTitle>Resources</DivTitle>
-            <div><a href="#id">FAQ</a></div>
-            <div><a href="#id">Advertise on Ecosia</a></div>
-            <div><a href="#id">Financial reports</a></div>
-            <div><a href="#id">Blog</a></div>
-            <div><a href="#id">Shop</a></div>
-            <div><a href="#id">Press</a></div>
-            <div><a href="#id">Jobs</a></div>
-          </div>
-          <div>
-            <DivTitle>Imprint</DivTitle>
+        </Grid>
+        <Grid item xs={8} xl={8}>
+          <div className={styles.container}>
             <div>
-              Ecosia GmbH
-              Schinkestraße 9
-              <br/>
-              12047 Berlin, Germany
-              <br/>
-              Chairman Christian Kroll
+              <div className={styles.title}>SITEMAP</div>
+              <div>
+                <a href="#id">News</a>
+              </div>
+              <div>
+                <a href="#id">How Ecosia works</a>
+              </div>
+              <div>
+                <a href="#id">About us</a>
+              </div>
+              <div>
+                <a href="#id">Mobile app</a>
+              </div>
+              <div>
+                <a href="#id">Privacy</a>
+              </div>
+              <div>
+                <a href="#id">Settings</a>
+              </div>
+            </div>
+            <div>
+              <div className={styles.title}>Resources</div>
+              <div>
+                <a href="#id">FAQ</a>
+              </div>
+              <div>
+                <a href="#id">Advertise on Ecosia</a>
+              </div>
+              <div>
+                <a href="#id">Financial reports</a>
+              </div>
+              <div>
+                <a href="#id">Blog</a>
+              </div>
+              <div>
+                <a href="#id">Shop</a>
+              </div>
+              <div>
+                <a href="#id">Press</a>
+              </div>
+              <div>
+                <a href="#id">Jobs</a>
+              </div>
+            </div>
+            <div>
+              <div className={styles.title}>Imprint</div>
+              <div>
+                Ecosia GmbH Schinkestraße 9
+                <br />
+                12047 Berlin, Germany
+                <br />
+                Chairman Christian Kroll
+              </div>
             </div>
           </div>
-        </DivContainer>
+        </Grid>
+        <Grid item xs={2} xl={2}>
+          <div className={styles.title}>Apps</div>
+          <div>
+            <Image
+              className={styles["image-wrapper"]}
+              src={images.appStoreImage}
+              alt="app store"
+            />
+          </div>
+          <div>
+            <Image
+              className={styles["image-wrapper"]}
+              src={images.playStoreImage}
+              alt="play store"
+            />
+          </div>
+        </Grid>
       </Grid>
-      <Grid item xs={2} xl={2}>
-        <DivTitle>Apps</DivTitle>
-        <div><ImgWrapper src={images.appStoreImage} alt="app store"></ImgWrapper></div>
-        <div><ImgWrapper src={images.playStoreImage} alt="play store"></ImgWrapper></div>
-      </Grid>
-    </GridWrapper>
-  )
-}
+    </div>
+  );
+};
 
 export default Links;
